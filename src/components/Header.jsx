d0fc56e2c21f2507c9af067d5b2f864529666e09
Stanislav <s.rud@factor.corp>
2021-01-06 08:57:13 +0200

    create new components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Cyberpunk_2077_logo.svg/1280px-Cyberpunk_2077_logo.svg.png"
+                alt="logo"/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://wroom.ru/i/cars2/moskvich_2140_1_2.jpg" alt="img"/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My Posts
+                <div>New Post</div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
