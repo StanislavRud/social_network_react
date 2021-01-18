0de5292c41e7d50f5d86290e5cbe890f1c0cd0d1
Stanislav <s.rud@factor.corp>
2021-01-18 10:35:12 +0200

    add page dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import style from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div className={style.content}>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/1' activeClassName={style.activeLink}>Stas</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/2' activeClassName={style.activeLink}>Timoha</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/3' activeClassName={style.activeLink}>Dimon</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/4' activeClassName={style.activeLink}>Valera</NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hello</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>I am fine</div>
+                <div className={style.message}>OK</div>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import style from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={`${style.item} ${style.active}`}>
-                <a href='/profile'>Profile</a>
+            <div className={style.item}>
+                <NavLink to='/profile' activeClassName={style.activeLink}>Profile</NavLink>
             </div>
             <div className={style.item}>
-                <a href='/dialogs'>Messages</a>
+                <NavLink to='/dialogs' activeClassName={style.activeLink}>Messages</NavLink>
             </div>
             <div className={style.item}>
-                <a href='/news'>News</a>
+                <NavLink to='/news' activeClassName={style.activeLink}>News</NavLink>
             </div>
             <div className={style.item}>
-                <a href='/music'>Music</a>
+                <NavLink to='/music' activeClassName={style.activeLink}>Music</NavLink>
             </div>
             <div className={style.item}>
-                <a href='/settings'>Settings</a>
+                <NavLink to='/settings' activeClassName={style.activeLink}>Settings</NavLink>
             </div>
 
         </nav>
