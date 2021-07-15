4bfc2630a8b1b6c73eca2af2cd55c32105ed6113
Stanislav <rudstanislav1990@gmail.com>
2021-07-15 09:13:33 +0300

    created tests components

---
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {create} from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
+
+describe("ProfileStatus component", () => {
+    test("status from props should be in the state", () => {
+        const component = create(<ProfileStatus status="hello nigaas" />);
+        const instance = component.getInstance();
+        expect(instance.state.status).toBe("hello nigaas");
+    });
+
+    test("after creation span should be displayed", () => {
+        const component = create(<ProfileStatus status="hello nigaas" />);
+        const root = component.root;
+        let span = root.findByType('span')
+        expect(span).not.toBeNull();
+    });
+
+    test("after creation input should be not display", () => {
+        const component = create(<ProfileStatus status="hello nigaas" />);
+        const root = component.root;
+        expect(() => {
+            let input = root.findByType('input')
+        }).toThrow();
+    });
+
+
+    test("after creation span should be displayed", () => {
+        const component = create(<ProfileStatus status="hello nigaas" />);
+        const root = component.root;
+        let span = root.findByType('span')
+        expect(span.children[0]).toBe('hello nigaas');
+    });
+
+    test("input should be displayed in edit mode insted of span", () => {
+        const component = create(<ProfileStatus status="hello nigaas" />);
+        const root = component.root;
+        let span = root.findByType('span');
+        span.props.onDoubleClick();
+        let input = root.findByType('input');
+        expect(input.props.value).toBe('hello nigaas');
+    });
+
+    test("callback should be called", () => {
+        const mockCallback = jest.fn();
+        const component = create(<ProfileStatus status="hello nigaas" updateStatus={ mockCallback } />);
+        const instance = component.getInstance();
+        instance.deactivateEditMode()
+        expect(mockCallback.mock.calls.length).toBe(1);
+    });
+
+
+});
